b059a6c92b6507253a882941eda0786408ca3261
davidgbarbero <davidgbarbero@gmail.com>
2016-12-09 13:11:02 +0100

    Jquery

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
-
-  // your code will go here
+	$.ajax({
+		url:'https://www.codeschool.com/users/davidgbarbero.json',
+		dataType: 'jsonp',
+		success: function(response){ 
+			$.each(response.courses.completed, function(i,item){
+				$('#badges').append('<div class="course"><h3>' + item.title +'</h3><img src="' + item.badge+
+				'" /><a class="btn btn-primary" target="_blank" href="' + item.url + '">See Course</a></div>');
+				console.log(item.title);
+			});
+		}
+	})
 
 });
